502a58fb2901af5a9ba117041f0845613a4c051a
BetaCatPro <zj20162325@163.com>
2024-09-25 21:46:52 +0800

    feat: use

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,31 +12,6 @@ const { Title, Paragraph } = Typography
 const Home: FC = () => {
   const nav = useNavigate()
 
-  // useEffect(() => {
-  //   // fetch('/api/test')
-  //   //   .then(res => res.json())
-  //   //   .then(data => console.log('fetch data', data))
-  //   // mock.js 只能劫持 XMLHttpRequest ，不能劫持 fetch
-
-  //   // axios 内部使用 XMLHttpRequest API ，没用 fetch
-  //   axios.get('/api/test').then(res => console.log('axios data', res.data))
-  // }, [])
-
-  // useEffect(() => {
-  //   // fetch('/api/test')
-  //   //   .then(res => res.json())
-  //   //   .then(data => console.log('fetch data', data))
-  //   // axios.get('/api/test').then(res => console.log('axios data', res.data))
-  // })
-
-  // function clickHandler() {
-  //   // nav('/login')
-  //   nav({
-  //     pathname: '/login',
-  //     search: 'b=21',
-  //   })
-  // }
-
   return (
     <div className={styles.container}>
       <div className={styles.info}>
